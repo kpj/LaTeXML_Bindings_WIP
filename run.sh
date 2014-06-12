ca3d7e4f4bcabdbdd899fb1532680ea72000d39c
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-06-12 08:35:04 -0400

    Some fixes

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 set -e
 set -u
 
-[ ! -d "$1" ] && echo "Usage: $0 <directory>" && exit 1
+[[ ( "$#" -ne 1 ) || ( ! -d "$1" ) ]] && echo "Usage: $0 <directory>" && exit 1
 
 cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$cwd"
 
 dir="$1"
 tex_file="${dir%/}.tex"
 xml_file="${tex_file/%tex/xml}"
 pdf_file="${dir%/}.pdf"
 
-rm -f "$xml_file" "$pdf_file"
 
 cd "$dir" && \
+rm -f "$xml_file" "$pdf_file" && \
 pdflatex -file-line-error -halt-on-error "$tex_file" && \
 latexml --path="$cwd/bindings" --dest="$xml_file" "$tex_file"
