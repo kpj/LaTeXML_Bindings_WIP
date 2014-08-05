f654560571de7f9d9efeb4c2114d28b37b1c7497
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-08-05 11:00:13 -0400

    Now extracts glyphs from pfb

--- tools/extract_glyphs.py
+++ tools/extract_glyphs.py
@@ -3,27 +3,35 @@ call with ./fontforge -script $0
 fontforge module is provided
 """
 
-import os, shutil, sys
+import os, os.path, shutil, sys
 
 from fontforge import *
 
 
 if len(sys.argv) != 2:
-    print('Usage: %s <svg font>' % sys.argv[0])
+    print('Usage: %s <pfb font>' % sys.argv[0])
     sys.exit(1)
 fontfile = sys.argv[1]
 
-font = open(fontfile)
+pbf_font = open(fontfile)
 
+# prepare working environment
+font_dir = pbf_font.fontname
 try:
-    shutil.rmtree(font.fontname)
-except FileNotFoundError:
+    shutil.rmtree(font_dir)
+except shutil.FileNotFoundError:
     pass
 finally:
-    os.mkdir(font.fontname)
-    os.chdir(font.fontname)
+    os.mkdir(font_dir)
+    os.chdir(font_dir)
 
-print('Encoding: ' + font.encoding)
-for glyph in font.glyphs():
+# convert font
+svg_fontfile = '%s.svg' % pbf_font.fontname
+pbf_font.generate(svg_fontfile)
+svg_font = open(os.path.join(font_dir, svg_fontfile))
+
+# extract glyphs
+print('Encoding: ' + svg_font.encoding)
+for glyph in svg_font.glyphs():
     print(glyph.glyphname, glyph.boundingBox())
     glyph.export('%s.svg' % glyph.glyphname)
