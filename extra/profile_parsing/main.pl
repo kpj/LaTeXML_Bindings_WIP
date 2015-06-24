54deffb7230384d95f5948fb631547c1b5c67ff2
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-06-24 16:38:43 -0400

    Refactoring

--- extra/profile_parsing/main.pl
+++ extra/profile_parsing/main.pl
@@ -5,7 +5,6 @@ use Getopt::Long;
 use Data::Dumper;
 use YAML::Tiny;
 
-
 sub load_profile {
   my ($file) = @_;
   print STDERR "(Loading '$file'... ";
@@ -13,22 +12,22 @@ sub load_profile {
   my $config;
   eval { $config = YAML::Tiny->read($file)->[0] }
     or croak("\nInvalid profile '$@'");
-  
-  print STDERR ")\n"; 
+
+  print STDERR ")\n";
   return $config; }
 
 sub merge_profiles {
   my ($cfg_main, $cfg_sub) = @_;
-  
+
   for my $section (keys %$cfg_sub) {
-    for my $key (keys %{$$cfg_sub{$section}}) {
-      @{$$cfg_main{$section}{$key}}{keys %{$$cfg_sub{$section}{$key}}} = values %{$$cfg_sub{$section}{$key}}; } }
+    for my $key (keys %{ $$cfg_sub{$section} }) {
+      @{ $$cfg_main{$section}{$key} }{ keys %{ $$cfg_sub{$section}{$key} } } = values %{ $$cfg_sub{$section}{$key} }; } }
 
   return $cfg_main; }
 
 sub get_file_extension {
   my ($file) = @_;
-  my ($ext) = $file =~ /\.([^.]+)$/;
+  my ($ext)  = $file =~ /\.([^.]+)$/;
   return $ext; }
 
 sub usage {
@@ -38,14 +37,14 @@ sub main {
   # load commandline parameters
   my $opts = {};
   GetOptions(
-    "profile=s"     => \$$opts{profile},
+    "profile=s" => \$$opts{profile},
     "destination=s" => \$$opts{destination}) or usage();
 
   # load source file
   if (scalar @ARGV != 1) {
     usage(); }
   my $source = $ARGV[0];
-  
+
   # let the merging begin!
   our $cfg = load_profile("LaTeXML.ltxprofile");
 
@@ -57,8 +56,7 @@ sub main {
   my ($key, $value);
   while (($key, $value) = each %$opts) {
     if (defined $value) {
-      $$cfg{$key} = $value;
-  } }
+      $$cfg{$key} = $value; } }
 
   print Dumper $cfg; }
 
